6e576a491eeac2572a3d6b2c8f0812893e56583b
Andrew Artamonov <1218836+andrew-artamonov@users.noreply.github.com>
2024-07-30 18:30:38 +0300

    fix2

--- djangoseo/utils.py
+++ djangoseo/utils.py
@@ -50,7 +50,7 @@ def _pattern_resolve_to_name(pattern, path):
             name = pattern.name
         elif hasattr(pattern, '_callback_str'):
             name = pattern._callback_str
-        elif hasattr(pattern.callback,'func_name')
+        elif hasattr(pattern.callback,'func_name'):
             name = "%s.%s" % (pattern.callback.__module__, pattern.callback.func_name)
         return name
 
